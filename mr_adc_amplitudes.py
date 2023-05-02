0f6572b8b50523e037869a0c35dc2bab0546301c
Carlos Eduardo Vieira de Moura <carlosevmoura@gmail.com>
2023-05-02 19:55:25 -0400

    Added T matrix terms for valence orbitals

--- mr_adc_amplitudes.py
+++ mr_adc_amplitudes.py
@@ -160,9 +160,12 @@ def compute_t1_amplitudes(mr_adc):
             if nval > 0:
                 mr_adc.t1.vxee = np.ascontiguousarray(mr_adc.t1.ccee[ncvs:,:ncvs,:,:])
                 mr_adc.t1.xvee = np.ascontiguousarray(mr_adc.t1.ccee[:ncvs,ncvs:,:,:])
+                mr_adc.t1.vvee = np.ascontiguousarray(mr_adc.t1.ccee[ncvs:,ncvs:,:,:])
 
                 mr_adc.t1.vxea = np.ascontiguousarray(mr_adc.t1.ccea[ncvs:,:ncvs,:,:])
                 mr_adc.t1.xvea = np.ascontiguousarray(mr_adc.t1.ccea[:ncvs,ncvs:,:,:])
+
+                mr_adc.t1.vvea = np.ascontiguousarray(mr_adc.t1.ccea[ncvs:,ncvs:,:,:])
 
                 #TODO: Improve this
                 t1_ccea_aaa = (mr_adc.t1.ccea - mr_adc.t1.ccea.transpose(1,0,2,3))
@@ -172,10 +175,13 @@ def compute_t1_amplitudes(mr_adc):
                 mr_adc.t1.vxae = np.ascontiguousarray(t1_ccae[ncvs:,:ncvs,:,:])
                 mr_adc.t1.xvae = np.ascontiguousarray(t1_ccae[:ncvs,ncvs:,:,:])
 
+                mr_adc.t1.vvae = np.ascontiguousarray(t1_ccae[ncvs:,ncvs:,:,:])
+
                 mr_adc.t1.vaee = np.ascontiguousarray(mr_adc.t1.caee[ncvs:,:,:,:])
 
                 mr_adc.t1.xvaa = np.ascontiguousarray(mr_adc.t1.ccaa[:ncvs,ncvs:,:,:])
                 mr_adc.t1.vxaa = np.ascontiguousarray(mr_adc.t1.ccaa[ncvs:,:ncvs,:,:])
+                mr_adc.t1.vvaa = np.ascontiguousarray(mr_adc.t1.ccaa[ncvs:,ncvs:,:,:])
 
     e_corr = e_0p + e_p1p + e_m1p + e_0 + e_p1 + e_m1 + e_p2 + e_m2
     e_tot = mr_adc.e_casscf + e_corr
