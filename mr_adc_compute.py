2db5de7152c856d1550002aa3aeaa1374a236d94
Donna Odhiambo <odhiambo.5@osu.edu>
2025-03-20 13:59:50 -0400

    removed comments and unemployed functions

--- mr_adc_compute.py
+++ mr_adc_compute.py
@@ -16,6 +16,7 @@
 # Authors: Alexander Yu. Sokolov <alexander.y.sokolov@gmail.com>
 #          Carlos E. V. de Moura <carlosevmoura@gmail.com>
 #                  Ilia M. Mazin <ilia.mazin@gmail.com>
+#              Donna H. Odhiambo <donna.odhiambo@proton.me>
 #
 
 import sys
@@ -104,13 +105,6 @@ def kernel(mr_adc):
                                            tol = mr_adc.tol_e,
                                            tol_residual = mr_adc.tol_davidson)
 
-#    conv, E, U = mr_adc.interface.davidson_nosym(lambda xs: [apply_M(x) for x in xs], x0, precond,
-#                                           nroots = mr_adc.nroots,
-#                                           verbose = davidson_verbose,
-#                                           max_space = mr_adc.max_space,
-#                                           max_cycle = mr_adc.max_cycle,
-#                                           tol = mr_adc.tol_e,
-#                                           tol_residual = mr_adc.tol_davidson)
     mr_adc.log.timer("solving eigenvalue problem", *cput1)
 
     mr_adc.log.note("\n%s-%s excitation energies (a.u.):" % (mr_adc.method_type, mr_adc.method))
@@ -123,7 +117,6 @@ def kernel(mr_adc):
     # Compute transition moments and spectroscopic factors
     U = np.array(U)
 
-#    spec_intensity, X = None, None
     spec_intensity, X = compute_trans_properties(mr_adc, E, U)
 
     mr_adc.log.info("\n------------------------------------------------------------------------------")
@@ -171,62 +164,6 @@ def setup_davidson(mr_adc):
     elif mr_adc.method_type == "cvs-ee":
         # Compute h0-h0 block of the effective Hamiltonian matrix
         M_00 = mr_adc_cvs_ee.compute_M_00(mr_adc)
-
-#        # Transform to orthogonal basis
-#        M_00_horth = np.zeros((mr_adc.h0.dim, mr_adc.h_orth.dim))
-#        M_00_forth = np.zeros((mr_adc.h_orth.dim, mr_adc.h_orth.dim))
-#
-#        for i in range(mr_adc.h0.dim):
-#            M_00_horth[i, :] = mr_adc_cvs_ee.apply_S_12(mr_adc, M_00[i, :], transpose = True)
-#
-#        # Delete used matrix
-#        del M_00
-#
-#        for i in range(mr_adc.h0.dim):
-#            M_00_forth[:, i] = mr_adc_cvs_ee.apply_S_12(mr_adc, M_00_horth[:, i], transpose = True)
-#
-#        # Delete used matrix
-#        del M_00_horth
-#
-#        # Diagonalize M matrix
-#        val, vec = np.linalg.eigh(M_00_forth)
-#
-#        # Delete used matrix
-#        del M_00_forth
-#
-#        # Print excitation energies in a.u. and eV
-#        print ("\n%s-%s excitation energies (a.u.):" % (mr_adc.method_type, mr_adc.method))
-#        print(val.reshape(-1,1)[:mr_adc.nroots])
-#        print ("\n%s-%s excitation energies (eV):" % (mr_adc.method_type, mr_adc.method))
-#        val_ev = val * 27.2114
-#        print (val_ev.reshape(-1, 1)[:mr_adc.nroots])
-#        sys.stdout.flush()
-#
-#        # Zeroth order T matrix
-#        T = mr_adc_cvs_ee.compute_trans_moments(mr_adc)
-#
-#        dip_mom = mr_adc.dip_mom
-#        dip_mom = dip_mom.reshape(dip_mom.shape[0], dip_mom.shape[1] * dip_mom.shape[2])
-#
-#        X = np.dot(T, vec[:,:mr_adc.nroots])
-#
-#        # Delete used matrix
-#        del T
-#
-#        dX = np.dot(dip_mom, X)
-#        
-#        spec_intensity = np.sum(dX**2, axis=0)
-#        print ("\n%s-%s spectroscopic intensity:" % (mr_adc.method_type, mr_adc.method))
-#        print(spec_intensity.reshape(-1,1))
-#
-#        osc_strength = (2.0/3.0) * val[:mr_adc.nroots] * spec_intensity
-#        print ("\n%s-%s oscillator strength:" % (mr_adc.method_type, mr_adc.method))
-#        print(osc_strength.reshape(-1,1))
-#
-#        # Not producing physically meaningful results, Will try with full first-order T matrix
-#        ##mr_adc_cvs_ee.analyze_eigenvectors(mr_adc, val, spec_intensity, X)
-#
-#        exit()
 
     # Compute diagonal of the M matrix
     if mr_adc.method_type == "ip":
