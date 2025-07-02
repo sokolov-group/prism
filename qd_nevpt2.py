1336c47e9c94480c285bafcbc7c997b31babc6eb
Alexander Sokolov <sokolov.8@osu.edu>
2025-07-02 16:26:30 -0400

    Implemented memory handling for the EE integrals

--- qd_nevpt2.py
+++ qd_nevpt2.py
@@ -20,6 +20,7 @@ import numpy as np
 from functools import reduce
 
 import prism.lib.logger as logger
+import prism.lib.tools as tools
 
 def compute_energy(nevpt, e_diag, t1, t1_0):
 
@@ -45,14 +46,11 @@ def compute_energy(nevpt, e_diag, t1, t1_0):
     ## Two-electron integrals
     v_cace = nevpt.v2e.cace
     v_caca = nevpt.v2e.caca
-    v_ceae = nevpt.v2e.ceae
     v_ceaa = nevpt.v2e.ceaa
     v_caae = nevpt.v2e.caae
     v_caaa = nevpt.v2e.caaa
-    v_aeae = nevpt.v2e.aeae
     v_aaae = nevpt.v2e.aaae
 
-    # TODO: optimize memory usage by grouping terms that depend on the same amplitudes and freeing memory after use
     # Compute the effective Hamiltonian matrix elements
     for I in range(dim):
         for J in range(I):
@@ -65,9 +63,7 @@ def compute_energy(nevpt, e_diag, t1, t1_0):
             t1_caaa = t1[J].caaa
             t1_aaae = t1[J].aaae
             t1_ccae = t1[J].ccae
-            t1_caee = t1[J].caee
             t1_ccaa = t1[J].ccaa
-            t1_aaee = t1[J].aaee
 
             H_IJ  = einsum('ia,ixay,yx', h_ce, t1_caea, trdm_ca, optimize = einsum_type)
             H_IJ -= 1/2 * einsum('ia,ixya,yx', h_ce, t1_caae, trdm_ca, optimize = einsum_type)
@@ -80,8 +76,6 @@ def compute_energy(nevpt, e_diag, t1, t1_0):
             H_IJ -= einsum('ijxy,ixjz,yz', t1_ccaa, v_caca, trdm_ca, optimize = einsum_type)
             H_IJ += 1/2 * einsum('ijxy,iyjz,xz', t1_ccaa, v_caca, trdm_ca, optimize = einsum_type)
             H_IJ += 1/4 * einsum('ijxy,izjw,xyzw', t1_ccaa, v_caca, trdm_ccaa, optimize = einsum_type)
-            H_IJ += einsum('ixab,iayb,yx', t1_caee, v_ceae, trdm_ca, optimize = einsum_type)
-            H_IJ -= 1/2 * einsum('ixab,ibya,yx', t1_caee, v_ceae, trdm_ca, optimize = einsum_type)
             H_IJ += einsum('ixay,iazw,yzxw', t1_caea, v_ceaa, trdm_ccaa, optimize = einsum_type)
             H_IJ += einsum('ixay,iazy,zx', t1_caea, v_ceaa, trdm_ca, optimize = einsum_type)
             H_IJ -= 1/2 * einsum('ixay,iyza,zx', t1_caea, v_caae, trdm_ca, optimize = einsum_type)
@@ -102,7 +96,6 @@ def compute_energy(nevpt, e_diag, t1, t1_0):
             H_IJ += einsum('ixyz,iywz,wx', t1_caaa, v_caaa, trdm_ca, optimize = einsum_type)
             H_IJ -= 1/2 * einsum('ixyz,izwu,ywxu', t1_caaa, v_caaa, trdm_ccaa, optimize = einsum_type)
             H_IJ -= 1/2 * einsum('ixyz,izwy,wx', t1_caaa, v_caaa, trdm_ca, optimize = einsum_type)
-            H_IJ += 1/4 * einsum('xyab,zawb,zwxy', t1_aaee, v_aeae, trdm_ccaa, optimize = einsum_type)
             H_IJ -= 1/6 * einsum('xyza,wuva,zvwuxy', t1_aaae, v_aaae, trdm_cccaaa, optimize = einsum_type)
             H_IJ -= 1/6 * einsum('xyza,wuva,zvwuyx', t1_aaae, v_aaae, trdm_cccaaa, optimize = einsum_type)
             H_IJ -= 1/6 * einsum('xyza,wuva,zvwxuy', t1_aaae, v_aaae, trdm_cccaaa, optimize = einsum_type)
@@ -111,15 +104,44 @@ def compute_energy(nevpt, e_diag, t1, t1_0):
             H_IJ -= 1/6 * einsum('xyza,wuva,zvwyxu', t1_aaae, v_aaae, trdm_cccaaa, optimize = einsum_type)
             H_IJ += 1/2 * einsum('xyza,wzua,wuxy', t1_aaae, v_aaae, trdm_ccaa, optimize = einsum_type)
 
+            if ncore > 0 and nextern > 0 and ncas > 0:
+                chunks = tools.calculate_chunks(nevpt, nextern, [ncore, ncas, nextern], ntensors = 3)
+                for i_chunk, (s_chunk, f_chunk) in enumerate(chunks):
+                    cput1 = (logger.process_clock(), logger.perf_counter())
+                    nevpt.log.debug("t1.caee [%i/%i], chunk [%i:%i]", i_chunk + 1, len(chunks), s_chunk, f_chunk)
+
+                    t1_caee = t1[J].caee[:,:,s_chunk:f_chunk,:]
+                    v_ceae = nevpt.v2e.ceae[:,s_chunk:f_chunk,:,:]
+
+                    H_IJ += einsum('ixab,iayb,yx', t1_caee, v_ceae, trdm_ca, optimize = einsum_type)
+
+                    v_ceae = nevpt.v2e.ceae[:,:,:,s_chunk:f_chunk]
+
+                    H_IJ -= 1/2 * einsum('ixab,ibya,yx', t1_caee, v_ceae, trdm_ca, optimize = einsum_type)
+
+                    nevpt.log.timer_debug("contracting t1.xaee", *cput1)
+                    del (t1_caee, v_ceae)
+
+            if nextern > 0 and ncas > 0:
+                chunks = tools.calculate_chunks(nevpt, nextern, [ncas, ncas, nextern], ntensors = 3)
+                for i_chunk, (s_chunk, f_chunk) in enumerate(chunks):
+                    cput1 = (logger.process_clock(), logger.perf_counter())
+                    nevpt.log.debug("t1.aaee [%i/%i], chunk [%i:%i]", i_chunk + 1, len(chunks), s_chunk, f_chunk)
+
+                    t1_aaee = t1[J].aaee[:,:,s_chunk:f_chunk,:]
+                    v_aeae = nevpt.v2e.aeae[:,s_chunk:f_chunk,:,:]
+
+                    H_IJ += 1/4 * einsum('xyab,zawb,zwxy', t1_aaee, v_aeae, trdm_ccaa, optimize = einsum_type)
+
+                    nevpt.log.timer_debug("contracting t1.aeae", *cput1)
+                    del (t1_aaee, v_aeae)
 
             t1_caea = t1[I].caea
             t1_caae = t1[I].caae
             t1_caaa = t1[I].caaa
             t1_aaae = t1[I].aaae
             t1_ccae = t1[I].ccae
-            t1_caee = t1[I].caee
             t1_ccaa = t1[I].ccaa
-            t1_aaee = t1[I].aaee
 
             # 0.5 * < Psi_I | T+ * V | Psi_J >
             H_IJ += einsum('ia,ixay,xy', h_ce, t1_caea, trdm_ca, optimize = einsum_type)
@@ -133,8 +155,6 @@ def compute_energy(nevpt, e_diag, t1, t1_0):
             H_IJ -= einsum('ijxy,ixjz,zy', t1_ccaa, v_caca, trdm_ca, optimize = einsum_type)
             H_IJ += 1/2 * einsum('ijxy,iyjz,zx', t1_ccaa, v_caca, trdm_ca, optimize = einsum_type)
             H_IJ += 1/4 * einsum('ijxy,izjw,zwxy', t1_ccaa, v_caca, trdm_ccaa, optimize = einsum_type)
-            H_IJ += einsum('ixab,iayb,xy', t1_caee, v_ceae, trdm_ca, optimize = einsum_type)
-            H_IJ -= 1/2 * einsum('ixab,ibya,xy', t1_caee, v_ceae, trdm_ca, optimize = einsum_type)
             H_IJ += einsum('ixay,iazw,xwyz', t1_caea, v_ceaa, trdm_ccaa, optimize = einsum_type)
             H_IJ += einsum('ixay,iazy,xz', t1_caea, v_ceaa, trdm_ca, optimize = einsum_type)
             H_IJ -= 1/2 * einsum('ixay,iyza,xz', t1_caea, v_caae, trdm_ca, optimize = einsum_type)
@@ -155,7 +175,6 @@ def compute_energy(nevpt, e_diag, t1, t1_0):
             H_IJ += einsum('ixyz,iywz,xw', t1_caaa, v_caaa, trdm_ca, optimize = einsum_type)
             H_IJ -= 1/2 * einsum('ixyz,izwu,xuyw', t1_caaa, v_caaa, trdm_ccaa, optimize = einsum_type)
             H_IJ -= 1/2 * einsum('ixyz,izwy,xw', t1_caaa, v_caaa, trdm_ca, optimize = einsum_type)
-            H_IJ += 1/4 * einsum('xyab,zawb,xyzw', t1_aaee, v_aeae, trdm_ccaa, optimize = einsum_type)
             H_IJ -= 1/6 * einsum('xyza,wuva,yxuvwz', t1_aaae, v_aaae, trdm_cccaaa, optimize = einsum_type)
             H_IJ += 1/3 * einsum('xyza,wuva,yxuvzw', t1_aaae, v_aaae, trdm_cccaaa, optimize = einsum_type)
             H_IJ -= 1/6 * einsum('xyza,wuva,yxuwvz', t1_aaae, v_aaae, trdm_cccaaa, optimize = einsum_type)
@@ -163,6 +182,37 @@ def compute_energy(nevpt, e_diag, t1, t1_0):
             H_IJ -= 1/6 * einsum('xyza,wuva,yxuzvw', t1_aaae, v_aaae, trdm_cccaaa, optimize = einsum_type)
             H_IJ -= 1/6 * einsum('xyza,wuva,yxuzwv', t1_aaae, v_aaae, trdm_cccaaa, optimize = einsum_type)
             H_IJ += 1/2 * einsum('xyza,wzua,xywu', t1_aaae, v_aaae, trdm_ccaa, optimize = einsum_type)
+
+            if ncore > 0 and nextern > 0 and ncas > 0:
+                chunks = tools.calculate_chunks(nevpt, nextern, [ncore, ncas, nextern], ntensors = 3)
+                for i_chunk, (s_chunk, f_chunk) in enumerate(chunks):
+                    cput1 = (logger.process_clock(), logger.perf_counter())
+                    nevpt.log.debug("t1.caee [%i/%i], chunk [%i:%i]", i_chunk + 1, len(chunks), s_chunk, f_chunk)
+
+                    t1_caee = t1[I].caee[:,:,s_chunk:f_chunk,:]
+                    v_ceae = nevpt.v2e.ceae[:,s_chunk:f_chunk,:,:]
+
+                    H_IJ += einsum('ixab,iayb,xy', t1_caee, v_ceae, trdm_ca, optimize = einsum_type)
+
+                    v_ceae = nevpt.v2e.ceae[:,:,:,s_chunk:f_chunk]
+
+                    H_IJ -= 1/2 * einsum('ixab,ibya,xy', t1_caee, v_ceae, trdm_ca, optimize = einsum_type)
+
+                    del (t1_caee, v_ceae)
+
+            if nextern > 0 and ncas > 0:
+                chunks = tools.calculate_chunks(nevpt, nextern, [ncas, ncas, nextern], ntensors = 3)
+                for i_chunk, (s_chunk, f_chunk) in enumerate(chunks):
+                    cput1 = (logger.process_clock(), logger.perf_counter())
+                    nevpt.log.debug("t1.aaee [%i/%i], chunk [%i:%i]", i_chunk + 1, len(chunks), s_chunk, f_chunk)
+
+                    t1_aaee = t1[I].aaee[:,:,s_chunk:f_chunk,:]
+                    v_aeae = nevpt.v2e.aeae[:,s_chunk:f_chunk,:,:]
+
+                    H_IJ += 1/4 * einsum('xyab,zawb,xyzw', t1_aaee, v_aeae, trdm_ccaa, optimize = einsum_type)
+
+                    nevpt.log.timer_debug("contracting t1.aeae", *cput1)
+                    del (t1_aaee, v_aeae)
 
             h_eff[I, J] = H_IJ
             h_eff[J, I] = H_IJ
