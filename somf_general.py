9bbb8ddde012c4040d873a700b71807437584569
RajatShubhro <majumder.rajat071@gmail.com>
2025-10-11 21:37:10 -0700

    Add socutils import in somf_general.py

---
+++ somf_general.py
@@ -0,0 +1,26 @@
+# Copyright 2025 Prism Developers. All Rights Reserved.
+#
+# Licensed under the GNU General Public License v3.0;
+# you may not use this file except in compliance with the License.
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+# either express or implied.
+#
+# See the License file for the specific language governing
+# permissions and limitations.
+#
+# Available at https://github.com/sokolov-group/prism
+#
+# Authors: Carlos E. V. de Moura <carlosevmoura@gmail.com>
+#          Alexander Yu. Sokolov <alexander.y.sokolov@gmail.com>
+#
+
+import sys
+import os
+socutils_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'prism', 'socutils'))
+if socutils_path not in sys.path:
+    sys.path.insert(0, socuitls_path)
+
+import somf
