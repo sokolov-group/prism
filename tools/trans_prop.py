8a05785fd0ed66e5b88feea7626e7e7a444e37ed
Donna Odhiambo <donna.odhiambo@proton.me>
2026-03-18 19:24:57 -0400

    added function for computing NTOs

--- tools/trans_prop.py
+++ tools/trans_prop.py
@@ -17,6 +17,7 @@
 #          Alexander Yu. Sokolov <alexander.y.sokolov@gmail.com>
 #          James D. Serna <jserna456@gmail.com>
 #          Donna H. Odhiambo <donna.odhiambo@proton.me>
+#          Bryce Pickett <brycepickett22@outlook.com>
 #
 
 import os
@@ -114,3 +115,63 @@ def compute_dyson(interface, X):
     interface.log.note(f"Dyson orbitals written to {name}_dyson.molden")
 
     return dyson_mos
+
+def compute_ntos(interface, trdm, initial_state=0, target_state=1):
+    '''
+    Computes natural transition orbitals between two given states
+    given an interface object (source of MO coefficients
+    and PySCF mol object) and a transition density matrix.
+    '''
+    ## Needs mask dependent on nto_thresh
+
+    interface.log.info(f"\nComputing NTOs for State {initial_state} -> State {target_state}")
+
+    U, s, Vh =  np.linalg.svd(trdm, full_matrices = False)
+    V = Vh.T
+    weights = s**2
+
+    assert np.allclose(U.T @ U, np.eye(U.shape[1])), "U is not unitary"
+    assert np.allclose(Vh @ Vh.T, np.eye(Vh.shape[0])), "Vh is not unitary"
+
+    # NTO Metrics
+    omega = np.sum(weights)                # sum of NTO occupation numbers
+    w = weights / omega                    # normalized weights
+    PR = 1.0 / np.sum(w**2)                # NTO participation ratio
+    S = -np.sum(w * np.log(w + 1e-16))     # entanglement entropy
+    Z = np.exp(S)                          # number of entangled states
+
+    interface.log.info(f"State {initial_state} -> State {target_state}:")
+    interface.log.info(f"   Sum of SVs (Omega):               {omega: .6f}")
+    interface.log.info(f"   Participation ratio (PR_NTO):     {PR: .6f}")
+    interface.log.info(f"   Entanglement entropy (S_HE):      {S: .6f}")
+    interface.log.info(f"   Nr of entangled states (Z_HE):    {Z: .6f}")
+    interface.log.info(f"   Renormalized S_HE/Z_HE:  {S/Z:.6f} / {1.0: .6f}")
+
+    # MO to AO
+    C_hole = interface.mo @ U
+    C_particle = interface.mo @ V
+    n_nto = C_hole.shape[1]
+
+    # phase consistency (relative to largest-magnitude AO coefficient)
+    for k in range(n_nto):
+        idx = np.argmax(np.abs(C_hole[:, k]))
+        if C_hole[idx, k] < 0:
+            C_hole[:, k] *= -1
+            C_particle[:, k] *= -1
+
+    # Interleave pairs: [hole_0, particle_0, hole_1, particle_1, ...]
+    C_nto = np.zeros((C_hole.shape[0], 2*n_nto))
+    C_nto[:, 0::2] = C_hole
+    C_nto[:, 1::2] = C_particle
+    occ_nto = np.repeat(weights, 2)
+
+    # Save to molden
+    input_file = os.path.splitext(os.path.basename(sys.argv[0]))[0]
+    filename = f"{input_file}_nto_S{initial_state}_S{target_state}.molden"
+    with open(filename, "w") as f:
+        molden.header(interface.mol, f)
+        molden.orbital_coeff(interface.mol, f, C_nto, occ=occ_nto)
+
+    interface.log.note(f"NTOs written to {filename}")
+
+    return weights, U, Vh
