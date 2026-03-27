141e661d7bb54bc430b2e91174bd0d12a3283465
Donna Odhiambo <donna.odhiambo@proton.me>
2026-03-27 15:36:18 -0400

    updates to cvs-ip-mr-adc tests

--- tests/cvs-ip-mr-adc/05-mradc2-n2-66.py
+++ tests/cvs-ip-mr-adc/05-mradc2-n2-66.py
@@ -52,7 +52,7 @@ emc = mc.mc1step()[0]
 print("CASSCF energy: %f\n" % emc)
 
 # Run MR-ADC computation
-interface = prism.interface.PYSCF(mf, mc, opt_einsum = True)
+interface = prism.interface.PYSCF(mf, mc, backend = 'opt_einsum')
 mr_adc = prism.mr_adc.MRADC(interface)
 mr_adc.nroots = 12
 mr_adc.ncvs = 2
