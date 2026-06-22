75f3f74208dea9d1b0a6fed2de768da2c9225fc8
dickerson.423 <dickerson.423@asccbc-01181201.asc.ohio-state.edu>
2026-06-22 14:22:52 -0400

    fixed test files, removed some test files that were not needed

---
+++ tests/nevpt2/23-dfnevpt2-n2-ROHF.py
@@ -0,0 +1,71 @@
+# Copyright 2026 Prism Developers. All Rights Reserved.
+#
+# Licensed under the GNU General Public License v3.0;
+# you may not use this file except in compliance with the License.
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+# either express or implied.
+#
+# See the License file for the specific language governing
+# permissions and limitations.
+#
+# Available at https://github.com/sokolov-group/prism
+#
+# Authors: Alexander Yu. Sokolov <alexander.y.sokolov@gmail.com>
+#          Carlos E. V. de Moura <carlosevmoura@gmail.com>
+
+import unittest
+import numpy as np
+import pyscf.gto
+import pyscf.scf
+import pyscf.mcscf
+import prism.interface
+import prism.nevpt
+
+np.set_printoptions(linewidth=150, edgeitems=10, suppress=True)
+
+r = 1.098
+
+mol = pyscf.gto.Mole()
+mol.atom = [
+            ['N', (0.0, 0.0, -r/2)],
+            ['N', (0.0, 0.0,  r/2)]]
+mol.basis = 'aug-cc-pvdz'
+mol.symmetry = True
+mol.build()
+
+# ROHF calculation
+mf = pyscf.scf.ROHF(mol)
+mf.conv_tol = 1e-12
+
+ehf = mf.scf()
+print("SCF energy: %f\n" % ehf)
+
+
+# NEVPT2 calculation
+interface = prism.interface.PYSCF(mf, backend = 'opt_einsum').density_fit('aug-cc-pvdz-ri')
+nevpt = prism.nevpt.NEVPT(interface)
+nevpt.compute_singles_amplitudes = False
+nevpt.semi_internal_projector = "gs"
+nevpt.s_thresh_singles = 1e-6
+nevpt.s_thresh_doubles = 1e-10
+
+class KnownValues(unittest.TestCase):
+
+    def test_pyscf(self):
+        self.assertAlmostEqual(mf.e_tot, -108.960608507245, 6)
+
+    def test_prism(self):
+
+        e_tot, e_corr, osc = nevpt.kernel()
+
+        self.assertAlmostEqual(e_tot[0], -109.282738926072, 6)
+        self.assertAlmostEqual(e_corr[0],  -0.322130418827, 6)
+
+if __name__ == "__main__":
+    print("NEVPT2 test")
+    unittest.main()
+
+
